bf598a6720e01dea0b5c6703fc752a93f89f8b53
Furkan Kaya <furkankaya7519@gmail.com>
2023-02-12 23:07:59 +0300

    feat: Captured all elements in js

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const startButton = document.querySelector(".btn-danger");
+const fireParagraph = document.querySelector(".row").querySelectorAll(".col-5")[0].querySelector("p");
+const waterParagraph = document.querySelector(".row").querySelectorAll(".col-5")[1].querySelector("p");
+const infoParagraph = document.querySelector("#infoParagraph");
+const boxButtonsArray = document.querySelectorAll(".btn-outline-primary");
+
+startButton.addEventListener("click",deneme);
+
+function deneme(){
+   // waterParagraph.innerHTML = "🔥 1";
+   //infoParagraph.innerHTML = "Fur";
+
+//    for (const i of boxButtonsArray) {
+//        i.style.backgroundColor = "red";
+//    }
+}
